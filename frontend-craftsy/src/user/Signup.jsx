db5c9a4eaa22deeaf1d87c455b6729ff0226ed08
Jibran Hussain Mir <mirjibranhussain@gmail.com>
2023-02-21 19:51:28 +0530

    Signup style

--- frontend-craftsy/src/user/Signup.jsx
+++ frontend-craftsy/src/user/Signup.jsx
@@ -4,6 +4,7 @@ import "./css/Signup.css";
 import { userSignup } from "../auth/index";
 import Header from "../core/Header";
 import "../user/css/Signup.css";
+import SignupImg from "../assets/signup.png";
 
 const Signup = () => {
   const [userRegistration, setUserRegistration] = useState({
@@ -45,77 +46,79 @@ const Signup = () => {
   return (
     <>
       <Header />
+
       <div className="signup signup-container">
-        <div className="form-heading">
-          <h1>Sign Up</h1>
+        <img src={SignupImg} alt="qll" className="signup-image" />
+        <div>
+          {/* If any error occured */}
+          {userRegistration.error && (
+            <div>
+              <h3>{userRegistration.error}</h3>
+            </div>
+          )}
+
+          {/* If the From is submitted successfully */}
+          {userRegistration.success && (
+            <div className="form-container">
+              <h3>
+                New Account has been created Successfully.{" "}
+                <Link to="/signin">Signin</Link> to continue...
+              </h3>
+            </div>
+          )}
+
+          <form method="POST" onSubmit={handleSubmit}>
+            <label htmlFor="full-name" className="form-name">
+              Name
+            </label>
+            <br />
+            <br />
+            <input
+              className="form-input-name"
+              type="text"
+              name="name"
+              placeholder="Your Name"
+              value={userRegistration.name}
+              onChange={handleChange}
+              autoComplete="off"
+            />
+            <br />
+
+            <label htmlFor="email" className="form-email">
+              Email
+            </label>
+            <br />
+            <br />
+            <input
+              className="form-input-email"
+              type="text"
+              name="email"
+              placeholder="Your Email"
+              value={userRegistration.email}
+              onChange={handleChange}
+              autoComplete="off"
+            />
+            <br />
+
+            <label htmlFor="Password" className="form-password">
+              Password
+            </label>
+            <br />
+            <br />
+            <input
+              className="form-input-password"
+              type="password"
+              name="password"
+              placeholder="Your Password"
+              value={userRegistration.password}
+              onChange={handleChange}
+              autoComplete="off"
+            />
+            <br />
+
+            <button className="form-btn-submit">Submit</button>
+          </form>
         </div>
-        {/* If any error occured */}
-        {userRegistration.error && (
-          <div>
-            <h3>{userRegistration.error}</h3>
-          </div>
-        )}
-
-        {/* If the From is submitted successfully */}
-        {userRegistration.success && (
-          <div className="">
-            <h3>
-              New Account has been created Successfully.{" "}
-              <Link to="/signin">Signin</Link> to continue...
-            </h3>
-          </div>
-        )}
-
-        <form method="POST" onSubmit={handleSubmit}>
-          <label htmlFor="full-name" className="form-name">
-            Name
-          </label>
-          <br />
-          <br />
-          <input
-            className="form-input-name"
-            type="text"
-            name="name"
-            placeholder="Your Name"
-            value={userRegistration.name}
-            onChange={handleChange}
-            autoComplete="off"
-          />
-          <br />
-
-          <label htmlFor="email" className="form-email">
-            Email
-          </label>
-          <br />
-          <br />
-          <input
-            className="form-input-email"
-            type="text"
-            name="email"
-            placeholder="Your Email"
-            value={userRegistration.email}
-            onChange={handleChange}
-            autoComplete="off"
-          />
-          <br />
-
-          <label htmlFor="Password" className="form-password">
-            Password
-          </label>
-          <br />
-          <input
-            className="form-input-password"
-            type="password"
-            name="password"
-            placeholder="Your Password"
-            value={userRegistration.password}
-            onChange={handleChange}
-            autoComplete="off"
-          />
-          <br />
-
-          <button className="form-btn-submit">Submit</button>
-        </form>
       </div>
     </>
   );
